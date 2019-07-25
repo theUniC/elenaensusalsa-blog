1a80cf49b992ab98c9e5ad30bcfd9c78c28bb24c
theUniC <theunic@gmail.com>
2019-07-25 23:18:42 +0200

    Fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-
 #!/bin/sh
 
 if [ "`git status -s`" ]
@@ -14,7 +13,7 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
+git worktree add -B gh-pages public origin/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
