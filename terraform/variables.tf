e4927fcdf14a8dc993f77a858dbcc7faf8218d94
theUniC <theunic@gmail.com>
2019-07-30 19:30:13 +0200

    Infrastructure

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,3 @@
-variable "digitalocean_token" {
-    default = "7ec3f88aac889358d0eb12e4a4311e086666b8217a29c3b29180f68d7aebc44f"
-}
-
-variable "digitalocean_spaces_access_key" {
-    default = "HZ6LJYZGAAOSRF3C3GLB"
-}
-
-variable "digitalocean_spaces_secret_key" {
-    default = "VaxQUsXckpP5uvyEXR7oBlkgeFBdgQnSEhPLKeWOY+c"
-}
-
 variable "cloudflare_email" {
     default = "theunic@gmail.com"
 }
@@ -17,12 +5,3 @@ variable "cloudflare_email" {
 variable "cloudflare_token" {
     default = "78b23fc3ab994d292d4a97d3f015122d13000"
 }
-
-variable "github_token" {
-    default = "ee0a89d332c3c60e6b6637b43e668fb063cbeb23"
-}
-
-variable "mysql_root_password" {
-    default = "1234Abcd"
-}
-
